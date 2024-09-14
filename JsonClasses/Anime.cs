90c0e9141c4c1d41c8125a7728cd36818554584e
tikkikkit21 <tikkikkit21@gmail.com>
2024-09-14 00:34:03 -0400

    Clean up JSON classes

--- JsonClasses/Anime.cs
+++ JsonClasses/Anime.cs
@@ -2,5 +2,11 @@
 
 namespace SeiyuuSync.JsonClasses
 {
-    
+    public class Anime
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+    }
 }
--- JsonClasses/AnimeSearchResponse.cs
+++ JsonClasses/AnimeSearchResponse.cs
@@ -5,20 +5,6 @@ namespace SeiyuuSync.JsonClasses
     class AnimeSearchResponse
     {
         [JsonPropertyName("data")]
-        public List<Node> Data { get; set; } = new List<Node>();
-    }
-
-    public class Node
-    {
-        [JsonPropertyName("node")]
-        public Anime Anime { get; set; }
-    }
-
-    public class Anime
-    {
-        [JsonPropertyName("id")]
-        public int Id { get; set; }
-        [JsonPropertyName("title")]
-        public string Title { get; set; }
+        public List<Node> Nodes { get; set; } = new List<Node>();
     }
 }
