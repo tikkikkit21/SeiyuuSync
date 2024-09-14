faddab3c4bcdde973259e76666ff36c993bf9333
Pojoto <114115897+Pojoto@users.noreply.github.com>
2024-09-14 14:04:22 -0400

    some UI additions

--- SearchForm.cs
+++ SearchForm.cs
@@ -1,11 +1,14 @@
 ﻿using SeiyuuSync.JsonClasses;
 using SeiyuuSync.Utils;
 using System.Text.Json;
+using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace SeiyuuSync
 {
     public partial class SearchForm : Form
     {
+        bool split = false;
         public SearchForm()
         {
             InitializeComponent();
@@ -28,6 +31,65 @@ namespace SeiyuuSync
             foreach (Node node in response.Nodes)
             {
                 dgvAnimeList.Rows.Add(node.Anime.Id, node.Anime.Title);
+            }
+        }
+
+        private void dgvAnimeList_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private async void CompareButton_Click(object sender, EventArgs e)
+        {
+            if(!split)
+            {
+                tableLayoutPanel1.Controls.Remove(dgvAnimeList);
+                tableLayoutPanel1.Controls.Add(dgvAnimeList, 1, 1); // Move button to cell (2,2)
+                tableLayoutPanel1.SetColumnSpan(dgvAnimeList, 10);
+
+
+
+
+
+
+                //tableLayoutPanel1.Controls.Remove(dvgVoiceActors);
+                //tableLayoutPanel1.Controls.Add(dvgVoiceActors, 11, 1); // Move button to cell (2,2)
+                //tableLayoutPanel1.SetColumnSpan(dvgVoiceActors, 8);
+                //dvgVoiceActors.Visible = true;
+
+                int animeId = (int)dgvAnimeList.SelectedCells[colAnimeId.Index].Value;
+                //api stuff here where you fetch the voice actors associated with this anime (locally i think)
+
+
+                Dictionary<string, string[]> voiceActorData = new Dictionary<string, string[]>();
+                voiceActorData["Timothy Cui"] = new string[] { "Summoner's Rift", "Virginia Tech" };
+                voiceActorData["Mike"] = new string[] { "Peraton", "Virginia Tech" };
+
+
+
+
+                TreeView treeView = new TreeView
+                {
+                    Dock = DockStyle.Fill
+                };
+
+                // Add nodes to TreeView
+                foreach (var actor in voiceActorData)
+                {
+                    TreeNode actorNode = new TreeNode(actor.Key);
+                    foreach (var movie in actor.Value)
+                    {
+                        actorNode.Nodes.Add(new TreeNode(movie));
+                    }
+                    treeView.Nodes.Add(actorNode);
+                }
+
+                // Add TreeView to the form
+                tableLayoutPanel1.Controls.Add(treeView, 11, 1);
+                tableLayoutPanel1.SetColumnSpan(treeView, 8);
+
+                treeView.ExpandAll();
+
+                split = true;
             }
         }
     }
