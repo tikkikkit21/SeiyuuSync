9aa54f9f226b327171b98addd58d4efee3825927
Tikki <35748816+tikkikkit21@users.noreply.github.com>
2024-09-14 17:34:44 -0400

    Improve home screen (#10)

--- HomeForm.Designer.cs
+++ HomeForm.Designer.cs
@@ -31,49 +31,92 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeForm));
             SearchButton = new Button();
             Title = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label1 = new Label();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // SearchButton
             // 
-            SearchButton.Location = new Point(228, 160);
-            SearchButton.Margin = new Padding(2, 2, 2, 2);
+            SearchButton.Anchor = AnchorStyles.Top;
+            SearchButton.Font = new Font("Segoe UI", 12F);
+            SearchButton.Location = new Point(513, 568);
+            SearchButton.Margin = new Padding(2);
             SearchButton.Name = "SearchButton";
-            SearchButton.Size = new Size(78, 20);
+            SearchButton.Size = new Size(130, 32);
             SearchButton.TabIndex = 0;
-            SearchButton.Text = "Search";
+            SearchButton.Text = "Start Search";
             SearchButton.UseVisualStyleBackColor = true;
             SearchButton.Click += SearchButton_Click;
             // 
             // Title
             // 
             Title.AutoSize = true;
-            Title.Font = new Font("Segoe UI", 20F);
-            Title.Location = new Point(113, 88);
+            Title.BackColor = Color.Transparent;
+            Title.Dock = DockStyle.Fill;
+            Title.Font = new Font("Segoe UI", 36F);
+            Title.Location = new Point(102, 0);
             Title.Margin = new Padding(2, 0, 2, 0);
             Title.Name = "Title";
-            Title.Size = new Size(328, 37);
+            Title.Size = new Size(952, 150);
             Title.TabIndex = 1;
-            Title.Text = "Tikki's Terrific Voice Search";
+            Title.Text = "SeiyuuSync";
+            Title.TextAlign = ContentAlignment.MiddleCenter;
             Title.Click += Title_Click;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.BackColor = Color.Transparent;
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 100F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 100F));
+            tableLayoutPanel1.Controls.Add(SearchButton, 1, 2);
+            tableLayoutPanel1.Controls.Add(Title, 1, 0);
+            tableLayoutPanel1.Controls.Add(label1, 1, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 150F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 100F));
+            tableLayoutPanel1.Size = new Size(1156, 666);
+            tableLayoutPanel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F);
+            label1.Location = new Point(103, 150);
+            label1.Name = "label1";
+            label1.Size = new Size(945, 75);
+            label1.TabIndex = 2;
+            label1.Text = resources.GetString("label1.Text");
             // 
             // HomeForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(560, 270);
-            Controls.Add(Title);
-            Controls.Add(SearchButton);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1156, 666);
+            Controls.Add(tableLayoutPanel1);
+            DoubleBuffered = true;
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(2, 2, 2, 2);
+            Margin = new Padding(2);
             Name = "HomeForm";
-            Text = "HomeForm";
+            Text = "SeiyuuSync";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private Button SearchButton;
         private Label Title;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label1;
     }
 }
