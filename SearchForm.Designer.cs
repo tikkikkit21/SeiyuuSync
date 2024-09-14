2e3e4f5d86520c91fc7aa6744270eb53249b8995
Pojoto <114115897+Pojoto@users.noreply.github.com>
2024-09-13 23:08:16 -0400

    added search UI updated stuff

--- SearchForm.Designer.cs
+++ SearchForm.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "SearchForm";
+            textBox1 = new TextBox();
+            AddButton = new Button();
+            CompareButton = new Button();
+            dgvAnimeList = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dgvAnimeList).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.ForeColor = SystemColors.ScrollBar;
+            textBox1.Location = new Point(270, 74);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(267, 31);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "search anime...";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // AddButton
+            // 
+            AddButton.Location = new Point(200, 705);
+            AddButton.Name = "AddButton";
+            AddButton.Size = new Size(112, 34);
+            AddButton.TabIndex = 1;
+            AddButton.Text = "Add";
+            AddButton.UseVisualStyleBackColor = true;
+            AddButton.Click += button1_Click;
+            // 
+            // CompareButton
+            // 
+            CompareButton.Location = new Point(425, 705);
+            CompareButton.Name = "CompareButton";
+            CompareButton.Size = new Size(112, 34);
+            CompareButton.TabIndex = 3;
+            CompareButton.Text = "Compare";
+            CompareButton.UseVisualStyleBackColor = true;
+            // 
+            // dgvAnimeList
+            // 
+            dgvAnimeList.AllowUserToAddRows = false;
+            dgvAnimeList.AllowUserToDeleteRows = false;
+            dgvAnimeList.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvAnimeList.Columns.AddRange(new DataGridViewColumn[] { Column1 });
+            dgvAnimeList.Location = new Point(211, 178);
+            dgvAnimeList.Name = "dgvAnimeList";
+            dgvAnimeList.ReadOnly = true;
+            dgvAnimeList.RowHeadersVisible = false;
+            dgvAnimeList.RowHeadersWidth = 62;
+            dgvAnimeList.Size = new Size(360, 398);
+            dgvAnimeList.TabIndex = 4;
+            dgvAnimeList.CellContentClick += dataGridView1_CellContentClick_1;
+            // 
+            // Column1
+            // 
+            Column1.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column1.HeaderText = "Column1";
+            Column1.MinimumWidth = 8;
+            Column1.Name = "Column1";
+            Column1.ReadOnly = true;
+            // 
+            // SearchForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 761);
+            Controls.Add(dgvAnimeList);
+            Controls.Add(CompareButton);
+            Controls.Add(AddButton);
+            Controls.Add(textBox1);
+            Name = "SearchForm";
+            Text = "SearchForm";
+            ((System.ComponentModel.ISupportInitialize)dgvAnimeList).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Button AddButton;
+        private Button CompareButton;
+        private DataGridView dgvAnimeList;
+        private DataGridViewTextBoxColumn Column1;
     }
 }
--- SearchForm.cs
+++ SearchForm.cs
@@ -16,5 +16,30 @@ namespace SeiyuuSync
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
