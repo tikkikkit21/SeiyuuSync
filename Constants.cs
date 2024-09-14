fb14a031ef10a51769e7c1da5bdf956098bab587
tikkikkit21 <tikkikkit21@gmail.com>
2024-09-14 00:05:10 -0400

    Add appsettings.json file
    This contains our access token for MAL. The way it's currently set up is
    really bad, but it works for now.

---
+++ Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeiyuuSync
+{
+    class Constants
+    {
+        public static readonly string MAL_ROOT = "https://api.myanimelist.net/v2";
+        public static string ACCESS_TOKEN = "";
+
+    }
+}
