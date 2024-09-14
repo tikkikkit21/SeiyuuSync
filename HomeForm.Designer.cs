b0a781a774ca34ff723ec0f6e39976479ce58794
tikkikkit21 <tikkikkit21@gmail.com>
2024-09-14 17:25:38 -0400

    Add shrine as background image

--- HomeForm.Designer.cs
+++ HomeForm.Designer.cs
@@ -31,14 +31,17 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeForm));
             SearchButton = new Button();
             Title = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // SearchButton
             // 
-            SearchButton.Location = new Point(228, 160);
-            SearchButton.Margin = new Padding(2, 2, 2, 2);
+            SearchButton.Anchor = AnchorStyles.Top;
+            SearchButton.Location = new Point(531, 568);
+            SearchButton.Margin = new Padding(2);
             SearchButton.Name = "SearchButton";
-            SearchButton.Size = new Size(78, 20);
+            SearchButton.Size = new Size(93, 32);
             SearchButton.TabIndex = 0;
             SearchButton.Text = "Search";
             SearchButton.UseVisualStyleBackColor = true;
@@ -47,33 +50,59 @@
             // Title
             // 
             Title.AutoSize = true;
+            Title.BackColor = Color.Transparent;
+            Title.Dock = DockStyle.Fill;
             Title.Font = new Font("Segoe UI", 20F);
-            Title.Location = new Point(113, 88);
+            Title.Location = new Point(102, 150);
             Title.Margin = new Padding(2, 0, 2, 0);
             Title.Name = "Title";
-            Title.Size = new Size(328, 37);
+            Title.Size = new Size(952, 416);
             Title.TabIndex = 1;
-            Title.Text = "Tikki's Terrific Voice Search";
+            Title.Text = "SeiyuuSync";
+            Title.TextAlign = ContentAlignment.TopCenter;
             Title.Click += Title_Click;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.BackColor = Color.Transparent;
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 100F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 100F));
+            tableLayoutPanel1.Controls.Add(Title, 1, 1);
+            tableLayoutPanel1.Controls.Add(SearchButton, 1, 2);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 150F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 100F));
+            tableLayoutPanel1.Size = new Size(1156, 666);
+            tableLayoutPanel1.TabIndex = 2;
             // 
             // HomeForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(560, 270);
-            Controls.Add(Title);
-            Controls.Add(SearchButton);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1156, 666);
+            Controls.Add(tableLayoutPanel1);
+            DoubleBuffered = true;
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(2, 2, 2, 2);
+            Margin = new Padding(2);
             Name = "HomeForm";
             Text = "HomeForm";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private Button SearchButton;
         private Label Title;
+        private TableLayoutPanel tableLayoutPanel1;
     }
 }
