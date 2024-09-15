2e6c91fcbf9d3252277a3597cd7665b7bde7e658
Tikki <35748816+tikkikkit21@users.noreply.github.com>
2024-09-14 21:48:00 -0400

    Add anime list (#17)

---
+++ Forms/AnimeListForm.cs
@@ -0,0 +1,29 @@
+﻿using SeiyuuSync.JsonClasses;
+using SeiyuuSync.Utils;
+
+namespace SeiyuuSync.Forms
+{
+    public partial class AnimeListForm : Form
+    {
+        private static ApiController apiController;
+
+        public AnimeListForm()
+        {
+            apiController = new ApiController();
+            InitializeComponent();
+        }
+
+        private async void AnimeListForm_Load(object sender, EventArgs e)
+        {
+            Cursor cursor = Cursor.Current;
+            Cursor = Cursors.WaitCursor;
+            List<Anime> animes = await apiController.GetMyAnimes();
+            foreach (Anime anime in animes)
+            {
+                dgvAnimeList.Rows.Add(anime.Title);
+            }
+            dgvAnimeList.ClearSelection();
+            Cursor = cursor;
+        }
+    }
+}
---
+++ Forms/HomeForm.cs
@@ -0,0 +1,22 @@
+﻿using SeiyuuSync.Forms;
+
+namespace SeiyuuSync
+{
+    public partial class HomeForm : Form
+    {
+        public HomeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            new SearchForm().Show();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            (new AnimeListForm()).Show();
+        }
+    }
+}
